c803d45a46681e9847f696ab5465cc3de6f6114a
Karishma Patil <127650690+Karishmakp@users.noreply.github.com>
2024-06-26 12:14:07 +0530

    Add files via upload

---
+++ moms belief/Task - 25-06-24/index.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    const navIcons = document.querySelector('.nav-icons');
+    navLinks.classList.toggle('active');
+    navIcons.classList.toggle('active');
+}
+
+
